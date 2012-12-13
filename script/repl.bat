9f86b8dd8819119ba13bd44e4d13565f8bd6ef2e
Matt Kocubinski <mkocubinski@gmail.com>
2012-12-13 10:05:32 -0500

    Initial commit.

---
+++ script/repl.bat
@@ -0,0 +1,1 @@
+bin\Debug\Console.exe --clj
---
+++ src/Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using CommandLine;
+using clojure.lang.Hosting;
+
+namespace Console
+{
+    class Program
+    {
+        class Options
+        {
+            [Option("c", "clj", DefaultValue = false)]
+            public bool ClojureRepl { get; set; }
+        } 
+
+        static private readonly Options options = new Options();
+
+        static void Main(string[] args)
+        {
+            var cmdParser = new CommandLineParser();
+            cmdParser.ParseArguments(args, options);
+
+            Clojure.AddNamespaceDirectoryMapping("clojurewm", "clojurewm");
+
+            var replInit = "(use 'clojurewm.init)";
+
+            if (options.ClojureRepl)
+                Clojure.Require("clojure.main").main("-e", replInit, "-r");
+        }
+    }
+}
